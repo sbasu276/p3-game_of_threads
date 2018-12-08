4e719571556afaf3f532c43c36d6aabcaac17375
Soumen Basu <mail.basu.s@gmail.com>
2018-12-09 04:39:13 +0530

    server constants file as config

---
+++ src/server_const.py
@@ -0,0 +1,12 @@
+# Needs to be changed in the client deployment config
+SERVER_ID_MAP = {
+        1: ["127.0.0.1", 65201],
+        2: ["127.0.0.1", 65202], 
+        3: ["127.0.0.1", 65203],
+        4: ["127.0.0.1", 65204],
+        5: ["127.0.0.1", 65205],
+    }
+
+# IP and port for central lock server
+LOCK_SERVER_IP = ""
+LOCK_SERVER_PORT = 
